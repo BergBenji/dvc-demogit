4c261e612b9554764b81b870a096da0f936c5741
BergBenji <BergBenji@192.168.10.11>
2012-09-27 09:55:59 +0200

    First Commit

---
+++ mytestfile.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of mytestfile
+ * 
+ *
+ * @author BergBenji
+ */
+class mytestfile {
+
+    function __construct() {
+	
+    }
+
+}
+
+?>
