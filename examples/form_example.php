0d410e114d35652003680605de8ac9c050513b2e
BergBenji <BergBenji@192.168.10.11>
2012-10-09 09:26:12 +0200

    Form example extended

--- examples/form_example.php
+++ examples/form_example.php
@@ -35,12 +35,17 @@ class form_example {
 	    ->val('minimum', 10)	// checks if value is min x
 	    ->val('maximum', 100)	// checks if value is max x
 	    ->val('contains','string1;string2;string3;etc');	// check is value contains one of the stringparts string1 or string2 etc..
+	$form
+	    ->post('other_field')
+	    ->val('minlength', 12);
 
 	try {
 	    $form->submit(); // initialize the validation routines
 	    
 	    $fieldname = $form->fetch('fieldname'); // value wich is secured through the Form class
-	    $formvalues = $form->fetch();
+	    $other_field = $form->fetch('other_field'); // value wich is secured through the Form class
+	    
+	    $formvalues = $form->fetch(); // array('fieldname' => 'value, 'other_field' => 'value2');
 	    
 	} catch (Exception $exc) {
 	    $error_keys = $form->fetchErrors('keys'); // Only the keys
